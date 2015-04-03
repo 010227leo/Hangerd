4110e1fd5eb317c12541ef1fa83b9a33258557af
leo <leo010227@outlook.com>
2015-04-03 12:29:26 +0800

    add IRepositoryFactory

---
+++ src/Hangerd.EntityFramework/EfRepositoryFactory.cs
@@ -0,0 +1,14 @@
+﻿using Hangerd.Entity;
+using Hangerd.Repository;
+
+namespace Hangerd.EntityFramework
+{
+	public class EfRepositoryFactory : IRepositoryFactory
+	{
+		public virtual IRepository<TEntity> CreateRepository<TEntity>(IRepositoryContext context)
+			where TEntity : EntityBase
+		{
+			return new EfRepository<TEntity>(context);
+		}
+	}
+}
---
+++ src/Hangerd/Repository/IRepositoryFactory.cs
@@ -0,0 +1,10 @@
+﻿using Hangerd.Entity;
+
+namespace Hangerd.Repository
+{
+	public interface IRepositoryFactory
+	{
+		IRepository<TEntity> CreateRepository<TEntity>(IRepositoryContext context)
+			where TEntity : EntityBase;
+	}
+}
